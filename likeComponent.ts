6e9b54204c6619845f72ecaf7cf98a3bfb21ef48
farouksouei <farouksouei@gmail.com>
2022-02-28 22:40:49 +0100

    putting the selector

--- likeComponent.ts
+++ likeComponent.ts
@@ -1,4 +1,15 @@
 export class LikeComponent {
-  constructor(public likesCount:number, public isSelected:boolean) { }
   
+  constructor(public likesCount:number, public isSelected:boolean) { 
+  }
+  onClick(){
+    if (this.isSelected) {
+      this.likesCount--;
+      this.isSelected = false;
+    } else {
+      this.likesCount++;
+      this.isSelected = true;
+    }
+  }
+
 }
